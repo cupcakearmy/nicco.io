692312457896e14609f5257e6bf74134c7cf37f2
cupcakearmy <hi@nicco.io>
2020-07-24 15:30:46 +0200

    projects

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -2,4 +2,41 @@
   export let project
 </script>
 
-<div>{project.title}</div>
+<style>
+  h2 {
+    font-size: 2em;
+    margin-bottom: 0.75em;
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: baseline;
+  }
+
+  section {
+    margin-bottom: 4em;
+  }
+
+  ion-icon {
+    transform: translateY(0.25em);
+  }
+</style>
+
+<section>
+  <div>
+    <h2>{project.name}</h2>
+  </div>
+
+  <div>
+    <b>{project.title}</b>
+    <b>{project.date}</b>
+  </div>
+
+  <p>{project.body}</p>
+
+  <ion-icon name="link-outline" />
+  <a href={project.link}>{project.link.replace(/https?:\/\//, '')}</a>
+
+</section>
