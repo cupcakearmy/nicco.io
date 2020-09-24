7b8a74f4865b02cb8e5e1b5d419134d77c578e40
cupcakearmy <hi@nicco.io>
2020-09-24 19:43:06 +0200

    privacy from remote source

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -1,50 +1,19 @@
+<script context="module">
+  import { getOne } from '../lib/wp'
+
+  export async function preload() {
+    const data = await getOne('pages', { slug: 'privacy' })
+    return { data }
+  }
+</script>
+
 <script>
+  import WPAdapter from '../components/WPAdapter.svelte'
   import SimplePage from '../components/SimplePage.svelte'
+
+  export let data
 </script>
 
 <SimplePage title="Privacy Policy">
-  <h3>TLDR;</h3>
-  <ul>
-    <li>There is no personal data collected, everything is anonymized</li>
-    <li>No data is passed to any third party</li>
-  </ul>
-
-  <hr />
-
-  <!-- <h2>The Legal</h2> -->
-
-  <p>
-    Your privacy is important to us. It is our policy to respect your privacy regarding any information we may collect
-    from you across our website, https://nicco.io, and other sites we own and operate.
-  </p>
-  <p>
-    We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and
-    lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.
-  </p>
-  <p>
-    We only retain collected information for as long as necessary to provide you with your requested service. What data
-    we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised
-    access, disclosure, copying, use or modification.
-  </p>
-  <p>
-    We don’t share any personally identifying information publicly or with third-parties, except when required to by
-    law.
-  </p>
-  <p>
-    Our website may link to external sites that are not operated by us. Please be aware that we have no control over the
-    content and practices of these sites, and cannot accept responsibility or liability for their respective privacy
-    policies.
-  </p>
-
-  <p>
-    You are free to refuse our request for your personal information, with the understanding that we may be unable to
-    provide you with some of your desired services.
-  </p>
-
-  <p>
-    Your continued use of our website will be regarded as acceptance of our practices around privacy and personal
-    information. If you have any questions about how we handle user data and personal information, feel free to contact
-    us.
-  </p>
-  <p>This policy is effective as of 1 January 2019.</p>
+  <WPAdapter content={data.content} />
 </SimplePage>
