13eb767fa059aa59f2779a5bd729884cca4cf7e2
Niccolo Borgioli <hi@nicco.io>
2024-11-22 00:36:46 +0100

    delete old state

--- src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-export function readingTimeInMinutes(text: string, options: { wpm?: number } = {}): number {
-  const cleaned = text.replace(/(<.*?>)|(\\n)|(&#\d*?;)/g, '')
-  const words = cleaned.split(' ').length
-  return Math.round(words / (options.wpm ?? 200))
-}
