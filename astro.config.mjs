4817ccff2ac5781af643b9bc3716768a5737da43
cupcakearmy <hi@nicco.io>
2026-06-28 19:09:30 +0200

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
 // @ts-check
-import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import svelte from '@astrojs/svelte'
-import { defineConfig } from 'astro/config'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import Icons from 'unplugin-icons/vite'
-import { remarkReadingTime } from './readingTime'
+import { unified } from "@astrojs/markdown-remark";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { defineConfig } from "astro/config";
+import Icons from "unplugin-icons/vite";
+import { remarkReadingTime } from "./readingTime";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap(), svelte()],
   markdown: {
-    rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
-    remarkPlugins: [remarkReadingTime],
+    processor: unified({
+      rehypePlugins: [[rehypeAutolinkHeadings, { behavior: "wrap" }]],
+      remarkPlugins: [remarkReadingTime],
+    }),
   },
   vite: {
-    plugins: [Icons({ compiler: 'astro' })],
+    plugins: [Icons({ compiler: "astro" })],
   },
-})
+});
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
+import { z } from 'astro/zod'
+import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
-  type: 'content',
-  // Type-check frontmatter using a schema
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string().optional(),
-      // Transform string to Date object
       date: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
       coverImage: image().optional(),
@@ -16,7 +16,7 @@ const blog = defineCollection({
 })
 
 const page = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/page' }),
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
